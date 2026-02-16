95f603f053fd62dcfe16da6b6714bb0fc90d3624
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-02-16 11:00:13 +0100

    Repository terminado, BaseRepository a medias

---
+++ src/main/java/es/fplumara/dam1/prestamos/repository/BaseRepository.java
@@ -0,0 +1,35 @@
+package es.fplumara.dam1.prestamos.repository;
+
+import es.fplumara.dam1.prestamos.model.Identificable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class BaseRepository <T extends Identificable> implements Repository<T>{
+
+    Map<String, T> datos;
+
+    @Override
+    public void save(T elemento) {
+        datos.put(elemento.getId(), elemento);
+    }
+
+    @Override
+    public Optional<T> findById(String id) {
+        Optional<T> elemento = Optional.empty();
+        elemento = Optional.of(datos.get(id));
+
+        return elemento;
+    }
+
+    @Override
+    public List<T> listAll() {
+        return List.of();
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+}
