57fd9917a6c87fab4ce39fff3a165da42ae967c9
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-02-16 10:40:12 +0100

    Modelo terminado

---
+++ src/main/java/es/fplumara/dam1/prestamos/model/Material.java
@@ -0,0 +1,35 @@
+package es.fplumara.dam1.prestamos.model;
+
+import java.util.Set;
+
+public abstract class Material implements Identificable{
+    String id;
+    String nombre;
+    EstadoMaterial estado;
+    Set<String> etiquetas;
+
+    // METODO ABSTRACTO
+    abstract String getTipo();
+
+    // GETTERS Y SETTERS
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public EstadoMaterial getEstado() {
+        return estado;
+    }
+
+    public Set<String> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEstado(EstadoMaterial estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/es/fplumara/dam1/prestamos/model/Proyector.java
@@ -0,0 +1,10 @@
+package es.fplumara.dam1.prestamos.model;
+
+public class Proyector extends Material{
+    int lumens;
+
+    @Override
+    String getTipo() {
+        return "Proyector";
+    }
+}
